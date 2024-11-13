abe696f28a48568189c7d014afd51a090043c550
M0T3L <104972867+M0T3L@users.noreply.github.com>
2024-11-13 21:33:40 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,8 +1,6 @@
 function showForm(formId) {
-    // Tüm formları gizle
     document.querySelectorAll('form').forEach(form => form.classList.remove('active'));
     
-    // Seçilen formu göster
     document.getElementById(formId).classList.add('active');
 }
 
